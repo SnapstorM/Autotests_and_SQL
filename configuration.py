a0e508e64ad00ebbdff335fd94ea5f16d0c7307d
SnapstorM <106017967+SnapstorM@users.noreply.github.com>
2025-03-16 19:55:53 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,4 +1,3 @@
 URL_SERVICE = "https://73c426dc-9af5-436c-9afe-2245693a8c18.serverhub.praktikum-services.ru"
-CREATE_COURIER_PATH = "/api/v1/courier"
 CREATE_ORDER_PATH = "/api/v1/orders"
 FIND_ORDER_PATH = "/api/v1/orders/track?t="
