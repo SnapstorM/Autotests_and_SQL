c1ba5e32940b19ada137f7c7da6eac02bd679d1a
SnapstorM <106017967+SnapstorM@users.noreply.github.com>
2025-03-18 01:08:23 +0300

    Update find_order_test.py

--- find_order_test.py
+++ find_order_test.py
@@ -1,19 +1,11 @@
 # Максим Иванов, 27-я когорта - Финальный проект. Инженер по тестированию плюс
 
-import requests
-import configuration
+import scooter_requests
 import data
 
-def create_order(body): # Создание заказа
+def test_status_code():
 
-    return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER_PATH, json=body, headers=data.headers)
+    track = scooter_requests.create_order(data.order_data)
+    result = scooter_requests.find_order(track)
 
-def find_order(): #Проверка статуса
-    
-    # Трек-номер перевожу в строку и методом конкатенации присоединяю к URL
-    response = requests.get(configuration.URL_SERVICE + configuration.FIND_ORDER_PATH + str(create_order(data.order_data).json()['track']))
-
-    assert response.status_code == 200 # Проверка статуса заказа
-    print("Тест пройден!") # Выводится в случае, если статус заказа - 200
-
-find_order()
+    assert result.status_code == 200
