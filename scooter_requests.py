90acac306806bbb77f1182ba30d6387284f4a5c0
SnapstorM <106017967+SnapstorM@users.noreply.github.com>
2025-03-18 01:28:19 +0300

    Update scooter_requests.py

--- scooter_requests.py
+++ scooter_requests.py
@@ -5,9 +5,10 @@ import data
 def create_order(body): # Создание заказа
 
     response = requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER_PATH, json=body, headers=data.headers)
-    track = str(response.json()['track']) # Сохраняем трек-номер в переменную с типом данных "строка"
+    track = response.json()['track']
+
     return track
 
 def find_order(track): #Поиск заказа
 
-    return requests.get(configuration.URL_SERVICE + configuration.FIND_ORDER_PATH + track)
+    return requests.get(configuration.URL_SERVICE + configuration.FIND_ORDER_PATH + str(track))
